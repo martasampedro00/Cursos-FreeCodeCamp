1f53304c97073d1a1a6127ff2d512b85c58b06e2
martasampedro00 <martasampedro00@gmail.com>
2025-11-12 16:49:33 +0100

    Cambios segunda unidad

---
+++ Free Foundational C# with Microsoft Certification/2.imprimir-valores-literales.cs
@@ -0,0 +1,37 @@
+/*Console.WriteLine('b');
+Console.WriteLine("Hello World!");
+
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.2565M);
+
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+------------------------------------
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+
+firstName = "Liem";
+Console.WriteLine(firstName);
+
+firstName = "Isabella";
+Console.WriteLine(firstName);
+
+firstName = "Yasmin";
+Console.WriteLine(firstName);*/
+
+string nombre = "Bob";
+int numeroEntero = 3;
+float numeroDecimal = 34.3f;
+
+Console.Write("Hello, ");
+Console.Write(nombre);
+Console.Write("! You have ");
+Console.Write(numeroEntero);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(numeroDecimal);
+Console.Write(" celsius");
