7fa22736a896a62d11e6168b3b25a6f28b55847a
martasampedro00 <martasampedro00@gmail.com>
2025-11-12 16:02:10 +0100

    Ejercicio 1 C#

---
+++ Free Foundational C# with Microsoft Certification/1.escribir-el-primer-codigo.cs
@@ -0,0 +1,10 @@
+//Console.WriteLine("Hola Mundo!");
+
+//Console.WriteLine("Congratulations!");
+//Console.Write(" ");
+//Console.Write("You wrote your first lines of code.");
+
+Console.WriteLine("This is the first line.");
+Console.Write("This is");
+Console.Write(" ");
+Console.Write("the second line.");
