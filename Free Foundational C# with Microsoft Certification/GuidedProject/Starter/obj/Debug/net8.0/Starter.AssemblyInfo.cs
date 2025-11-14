50bfb5e93910a9ccdff469f25823d1e96ba8fb68
marsampe <marsampe@ucm.es>
2025-11-14 12:44:08 +0100

    MatricesAlumnos

--- Free Foundational C# with Microsoft Certification/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Free Foundational C# with Microsoft Certification/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d130559b1f64459573f0924f06cd35b4780af2fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b39f823b4d00dae985a12e88fe6d5ccabf0f4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
