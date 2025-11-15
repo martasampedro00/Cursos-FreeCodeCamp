a9d25b2e7e4d701b360d26cc9ec287639e8b4b82
martasampedro00 <martasampedro00@gmail.com>
2025-11-15 12:26:42 +0100

    project 1 Bloque 3

---
+++ Free Foundational C# with Microsoft Certification/Project1Bloque3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Introduzca un valor entre 5 y 10:");
+string? lectura;
+int numero = 0;
+bool valido = false;
+do
+{
+    lectura = Console.ReadLine();
+    valido = int.TryParse(lectura, out numero);
+
+    if (valido)
+    {
+        if(numero >=5 && numero <= 10)
+            valido = true;
+        else{
+            Console.WriteLine($"Has introducido {numero}. Introduce un numero entre 5 y 10.");
+            valido = false;
+        }
+           
+    }
+    else
+    {
+        Console.WriteLine("Valor no valido, vuelva a introducir otro:");
+        valido = false;
+    }
+        
+
+}while(valido == false);
+
+Console.WriteLine($"Valor {numero} es valido");
