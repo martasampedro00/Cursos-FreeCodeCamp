91ccd563ce2e12d257f1b2d76661a00b0b4730fb
martasampedro00 <martasampedro00@gmail.com>
2025-11-15 12:41:51 +0100

    Proyecto 2 Bloque 3

---
+++ Free Foundational C# with Microsoft Certification/Project2Bloque3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine($"Introduce tu rol (Administrador, Director o Usuario)");
+string? entrada;
+bool valido = false;
+do
+{
+    entrada = Console.ReadLine();
+    entrada = entrada.Trim().ToLower();
+    if(entrada == "administrador" || entrada== "director" || entrada == "usuario")
+    {
+        valido = true;
+    }
+    else
+    {
+        Console.WriteLine($"El rol que has introducido, {entrada} no es valido. Introduce un rol valido (Administrador, Director o Usuario)");
+    }
+}while(valido == false);
+
+Console.WriteLine($"Tu rol ({entrada}) ha sido aceptado");
