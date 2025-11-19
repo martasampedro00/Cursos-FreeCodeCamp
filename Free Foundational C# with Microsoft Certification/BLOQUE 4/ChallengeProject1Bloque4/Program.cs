813b45e8cbce996714dae2d6502cfae3e19f8642
marsampe <marsampe@ucm.es>
2025-11-19 13:56:27 +0100

    Proyecto Desafio Bloque 4

---
+++ Free Foundational C# with Microsoft Certification/BLOQUE 4/ChallengeProject1Bloque4/Program.cs
@@ -0,0 +1,21 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+int indexInicio = input.IndexOf("<span>");
+int indexFinal = input.IndexOf("</span>");
+
+int posicionesTotales = indexFinal - indexInicio - 6;
+
+quantity = input.Substring(indexInicio + 6, posicionesTotales);
+
+indexInicio = input.IndexOf("<div>");
+indexFinal = input.IndexOf("</div>");
+
+posicionesTotales = indexFinal - indexInicio - 5;
+
+output = input.Substring(indexInicio + 5, posicionesTotales);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
